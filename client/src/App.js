af2146c706408e0335e942843561cd134a335df2
Maria McBride <mariacmcbride2@gmail.com>
2022-02-01 12:11:04 -0800

    updated changes

--- client/src/App.js
+++ client/src/App.js
@@ -27,7 +27,6 @@ import Create from './views/RecipeCRUD/Create';
 import Detail from './views/RecipeCRUD/Detail';
 import Update from './views/RecipeCRUD/Update';
 
-<<<<<<< HEAD
 // Views Imports
 import LandingPage from './views/LandingPage';
 import Main from './views/Main';
@@ -37,17 +36,6 @@ import DetailUser from './views/UserCRUD/DetailUser';
 import EditUser from './views/UserCRUD/EditUser';
 
 // Styling Imports
-=======
-// Styling Imports
-
-import Dashboard from './views/Dashboard';
-
-import ViewAllUsers from './views/UserCRUD/ViewAllUsers';
-import DetailUser from './views/UserCRUD/DetailUser';
-import EditUser from './views/UserCRUD/EditUser';
-import Main from './views/Main';
-import LandingPage from './views/LandingPage';
->>>>>>> aadf6ef79dbe36e3361c6836e15b00ff54d71144
 import ToggleColorMode from './components/Themes/ToggleDarkMode';
 import './App.css';
 
@@ -82,8 +70,6 @@ export default () => {
               <Dashboard />
             </Route>
             <Route exact path='/dashboard/edit/:id'>
-
-<<<<<<< HEAD
               <ManagePostsTab />
             </Route>
             <Route exact path='/dashboard/users/:id'>
@@ -123,71 +109,6 @@ export default () => {
             </Route>
           </Switch>
         </BrowserRouter>
-=======
-
-      <BrowserRouter>
-        <Switch>
-          {/* Login & Registration Routes */}
-          <Route exact path='/'>
-            <LandingPage/>
-          </Route>
-          <Route exact path='/login'>
-            <LogAndRegView>
-              <Login />
-            </LogAndRegView>
-          </Route>
-          <Route exact path='/register'>
-            <LogAndRegView>
-              <Register />
-            </LogAndRegView>
-          </Route>
-          
-          {/* Dashboard Routes */}
-          <Route exact path='/dashboard/:id'>
-            <Dashboard/>
-          </Route>
-          <Route exact path='/dashboard/edit/:id'>
-            
-            <ManagePostsTab/>
-          </Route>
-          <Route exact path='/dashboard/users/:id'>
-            <UsersLiked/>
-          </Route>
-          <Route exact path='/dashboard/favorites/:id'>
-            <FavoriteRecipes/>
-          </Route>
-          
-          {/* User Routes */}
-          <Route exact path='/users'>
-           <ViewAllUsers/>
-          </Route>
-          <Route exact path='/user/:id'>
-            <DetailUser/>
-          </Route>
-          <Route exact path='/user/edit/:id'>
-            <EditUser/>
-          </Route>
-          {/* Recipe Routes */}
-          <Route exact path='/recipes'>
-            <Main/>
-          </Route>
-          <Route exact path='/:category/recipes'>
-            <CategoryViewAll />
-          </Route>
-          <Route exact path='/recipe/new'>
-            <Create />
-          </Route>
-          <Route exact path='/recipe/:id'>
-            <Detail />
-          </Route>
-          <Route exact path='/recipe/edit/:id'>
-            <Update />
-          </Route>
-
-        </Switch>
-      </BrowserRouter>
-      
->>>>>>> aadf6ef79dbe36e3361c6836e15b00ff54d71144
       </ToggleColorMode>
     </div>
   );
