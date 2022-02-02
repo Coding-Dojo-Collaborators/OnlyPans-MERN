d8cab3efb22d4c6de35a8b589a8f4ce1871fc45e
Eric <erictoribio2010@gmail.com>
2022-02-02 18:54:58 -0500

    recipe

--- client/src/components/Dashboard/DashboardBody.jsx
+++ client/src/components/Dashboard/DashboardBody.jsx
@@ -5,9 +5,9 @@ import { useState , useEffect} from "react"
 // import Avatar from '@mui/material/Avatar';
 // import { Row, Item } from '@mui-treasury/components/flex';
 // import { Info, InfoTitle } from '@mui-treasury/components/info';
-import './Dashboard.css';
+
 import axios from 'axios';
-import React, { useState } from 'react';
+
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import ChromeTabs from './ChromeTabs';
@@ -21,40 +21,53 @@ export default ({user}) => {
     axios.get(`http://localhost:8000/api/recipe/user/${user}`)
     .then(res => {
       console.log(res.data);
-      // setRecipe(res.data)
+      setRecipe(res.data)
     }).catch(err => console.log(err))
   },[])
   
 
 
-
+ console.log(recipe);
   return (
-    <div className='d-flex justify-content-center'>
-      <AppBar
-        position={''}
-        elevation={0}
-        style={{ backgroundColor: '#E6E8EB', width: 900 }}
-      >
-        <Toolbar>
-          <ChromeTabs
-            style={{ alignSelf: 'flex-end' }}
-            tabs={[
-              { label: 'Latest Published' },
-              { label: 'Manage Posts' },
-            ]}
-            tabStyle={{
-              bgColor: '#E6E8EB',
-              selectedBgColor: '#ffffff',
-              color: 'rgba(0,0,0,0.87)',
-            }}
-            tabProps={{
-              disableRipple: true,
-            }}
-            value={index}
-            onChange={(e, i) => setIndex(i)}
-          />
-        </Toolbar>
-      </AppBar>
+    <div>
+      <div className='d-flex justify-content-center'>
+        <AppBar
+          position={''}
+          elevation={0}
+          style={{ backgroundColor: '#E6E8EB', width: 900 }}
+        >
+          <Toolbar>
+            <ChromeTabs
+              style={{ alignSelf: 'flex-end' }}
+              tabs={[
+                { label: 'Latest Published' },
+                { label: 'Manage Posts' },
+              ]}
+              tabStyle={{
+                bgColor: '#E6E8EB',
+                selectedBgColor: '#ffffff',
+                color: 'rgba(0,0,0,0.87)',
+              }}
+              tabProps={{
+                disableRipple: true,
+              }}
+              value={index}
+              onChange={(e, i) => setIndex(i)}
+            />
+          </Toolbar>
+        </AppBar>
+      </div>
+        <div>
+          {
+            recipe.map((recipe, i) => {
+              return(
+                <p>
+                  {recipe.name}
+                </p>
+              )
+            })
+          }
+        </div>
     </div>
   );
 };
