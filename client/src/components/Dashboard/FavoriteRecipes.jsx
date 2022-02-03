bcfbbcb2bfb489b6be4fbb16490b3d114e95e8e5
Nate Trichanh <natetrichanh@gmail.com>
2022-02-03 15:02:00 -0800

    favorite recipes

--- client/src/components/Dashboard/FavoriteRecipes.jsx
+++ client/src/components/Dashboard/FavoriteRecipes.jsx
@@ -2,48 +2,37 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { useHistory } from 'react-router-dom';
 
-const FavoriteRecipes = ({favoriteRecipes}) => {
-  
-  const [recipes, setRecipes] = useState([])
-  
-  
-  
+const FavoriteRecipes = ({ favoriteRecipes }) => {
+  const [recipes, setRecipes] = useState([]);
+
   console.log(favoriteRecipes);
   useEffect(() => {
 
-    favoriteRecipes.map((item,i)=>{
-      axios.get(`http://localhost:8000/api/recipe/favorites`,
+    axios.get(`http://localhost:8000/api/recipe/`,
       favoriteRecipes
-      )
+    )
       .then(res => {
         console.log(res.data)
-        setRecipes( res.data.results)
-    
+        setRecipes(res.data)
       })
       .catch(err => {
         console.log(err)
       })
-  
-    })
-    
-  },[])
 
+  }, [])
 
-  
-    
- 
   console.log(recipes)
+
   return (
     <div className='showOne'>
-      { 
-      recipes.map((recipe, index) => {
+      {recipes.filter(function (currentElement) {
         return (
-          <p key = {index}>
-            <img className='showOne' src = {recipe.image} />
-            {recipe.name}
-          </p>
+          favoriteRecipes.map((recipe, i) => {
+            recipes._id == recipe
+          })
         )
-      })}
+      })
+      }
     </div>
   )
 };
