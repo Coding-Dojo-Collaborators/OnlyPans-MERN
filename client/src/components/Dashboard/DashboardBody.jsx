cff93b259cad2640a1350b2b694a57920c458b99
Maria McBride <mariacmcbride2@gmail.com>
2022-02-02 17:30:18 -0800

    Stuff

--- client/src/components/Dashboard/DashboardBody.jsx
+++ client/src/components/Dashboard/DashboardBody.jsx
@@ -5,55 +5,35 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import PropTypes from 'prop-types';
 import SwipeableViews from 'react-swipeable-views';
-import { useTheme } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import { createStyles, makeStyles } from '@mui/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import { Link } from 'react-router-dom';
 // import Avatar from '@mui/material/Avatar';
 // import { Row, Item } from '@mui-treasury/components/flex';
 // import { Info, InfoTitle } from '@mui-treasury/components/info';
 
 const DashboardBody = ({ user, children, value, index, ...other }) => {
-<<<<<<< HEAD
-  return (
-    <div className=''>
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`full-width-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
-      </div>
-=======
-  // eslint-disable-next-line no-unused-vars
-  
   return (
     <div
-    role="tabpanel"
-    hidden={value !== index}
-    id={`full-width-tabpanel-${index}`}
-    aria-labelledby={`simple-tab-${index}`}
-    {...other}
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
     >
       {value === index && (
         <Box sx={{ p: 3 }}>
           <Typography>{children}</Typography>
         </Box>
       )}
->>>>>>> 12062ceca302a27552b66acaf152ff07eaed8991
     </div>
   );
 }
-
 
 DashboardBody.propTypes = {
   children: PropTypes.node,
@@ -68,30 +48,21 @@ function a11yProps(index) {
   };
 }
 
-<<<<<<< HEAD
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    root: {
+      backgroundColor: theme.palette.red,
+    },
+  }),
+);
+
+const theme = createTheme();
+
 export default function BasicTabs({ user }) {
-  // eslint-disable-next-line no-unused-vars
   const [recipe, setRecipe] = useState([]);
   const [value, setValue] = React.useState(0);
-  const theme = useTheme();
+  const classes = useStyles();
 
-  useEffect(() => {
-    axios.get(`http://localhost:8000/api/recipe/user/${user}`)
-      .then(res => {
-        console.log(res.data);
-        setRecipe(res.data)
-      }).catch(err => console.log(err))
-  }, [user]);
-  console.log(recipe);
-
-=======
-export default function BasicTabs({user}) {
-  
-  
-  const [value, setValue] = React.useState(0);
-  const theme = useTheme();
-  const [recipe, setRecipe] = useState([]);
-  
   useEffect(() => {
     axios.get(`http://localhost:8000/api/recipe/user/${user}`)
       .then(res => {
@@ -99,8 +70,7 @@ export default function BasicTabs({user}) {
         setRecipe(res.data)
       }).catch(err => console.log(err))
   }, [user]);
-  
->>>>>>> 12062ceca302a27552b66acaf152ff07eaed8991
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
@@ -108,76 +78,49 @@ export default function BasicTabs({user}) {
   const handleChangeIndex = (index) => {
     setValue(index);
   };
-  console.log(recipe[1])
+  console.log(recipe[1]);
+
   return (
-    <div className='d-flex justify-content-center'>
-      <Box sx={{ bgcolor: 'background.paper', width: 750 }}>
-        <AppBar position="static">
-          <Tabs
-            value={value}
-            onChange={handleChange}
-            indicatorColor="secondary"
-            bgColor=""
-            textColor="inherit"
-            variant=""
-            aria-label="basic tabs example"
-          >
-            <Tab label="Item One" {...a11yProps(0)} />
-            <Tab label="Item Two" {...a11yProps(1)} />
-          </Tabs>
-        </AppBar>
-        <SwipeableViews
-          axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-          index={value}
-          onChangeIndex={handleChangeIndex}
-        >
-<<<<<<< HEAD
-          <DashboardBody value={value} index={0} dir={theme.direction}>
-            <div>
-              {
-                recipe.map((recipe, i) => {
-                  return (
-                    <p key={i}>
-                      {recipe.name}
-                    </p>
-                  );
-                })
-              }
-            </div>
-          </DashboardBody>
-          <DashboardBody value={value} index={1} dir={theme.direction}>
-            Item Two
-          </DashboardBody>
-        </SwipeableViews>
-      </Box>
-    </div>
-=======
-          <Tab label="Item One" {...a11yProps(0)} />
-          <Tab label="Item Two" {...a11yProps(1)} />
-          
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-        index={value}
-        onChangeIndex={handleChangeIndex}
-      >
-          <div>
-            {
-              recipe.map((recipe, i) => {
-                return (
-                  <DashboardBody value={value} index={0} dir={theme.direction}>
-                    {recipe.name}
-            </DashboardBody>
-                );
-              })
-            }
-          </div>
-        <DashboardBody value={value} index={1} dir={theme.direction}>
-          Item Two
-        </DashboardBody>
-      </SwipeableViews>
-    </Box>
->>>>>>> 12062ceca302a27552b66acaf152ff07eaed8991
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <div className='d-flex justify-content-center'>
+          <Box sx={{ bgcolor: 'background.paper', width: 750 }}>
+            <AppBar position="static">
+              <Tabs
+                value={value}
+                onChange={handleChange}
+                indicatorColor="secondary"
+                textColor="inherit"
+                variant=""
+                aria-label="basic tabs example"
+              >
+                <Tab label="Item One" {...a11yProps(0)} />
+                <Tab label="Item Two" {...a11yProps(1)} />
+              </Tabs>
+            </AppBar>
+            <SwipeableViews
+              axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+              index={value}
+              onChangeIndex={handleChangeIndex}
+            >
+              <div>
+                {
+                  recipe.map((recipe, i) => {
+                    return (
+                      <DashboardBody value={value} index={0} dir={theme.direction}>
+                        {recipe.name}
+                      </DashboardBody>
+                    );
+                  })
+                }
+              </div>
+              <DashboardBody value={value} index={1} dir={theme.direction}>
+                Item Two
+              </DashboardBody>
+            </SwipeableViews>
+          </Box>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 };
