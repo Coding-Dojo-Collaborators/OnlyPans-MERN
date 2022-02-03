8576f230d6a66f9e1128b682460dba44fccf29dd
Eric <erictoribio2010@gmail.com>
2022-02-02 19:41:59 -0500

    carousel fix

--- client/src/components/Dashboard/DashboardBody.jsx
+++ client/src/components/Dashboard/DashboardBody.jsx
@@ -27,47 +27,48 @@ export default ({user}) => {
   
 
 
- console.log(recipe);
-  return (
-    <div>
-      <div className='d-flex justify-content-center'>
-        <AppBar
-          position={''}
-          elevation={0}
-          style={{ backgroundColor: '#E6E8EB', width: 900 }}
-        >
-          <Toolbar>
-            <ChromeTabs
-              style={{ alignSelf: 'flex-end' }}
-              tabs={[
-                { label: 'Latest Published' },
-                { label: 'Manage Posts' },
-              ]}
-              tabStyle={{
-                bgColor: '#E6E8EB',
-                selectedBgColor: '#ffffff',
-                color: 'rgba(0,0,0,0.87)',
-              }}
-              tabProps={{
-                disableRipple: true,
-              }}
-              value={index}
-              onChange={(e, i) => setIndex(i)}
-            />
-          </Toolbar>
-        </AppBar>
-      </div>
-        <div>
-          {
-            recipe.map((recipe, i) => {
-              return(
-                <p>
-                  {recipe.name}
-                </p>
-              )
-            })
-          }
+ 
+   console.log(recipe);
+    return (
+      <div>
+        <div className='d-flex justify-content-center'>
+          <AppBar
+            position={''}
+            elevation={0}
+            style={{ backgroundColor: '#E6E8EB', width: 900 }}
+          >
+            <Toolbar>
+              <ChromeTabs
+                style={{ alignSelf: 'flex-end' }}
+                tabs={[
+                  { label: 'Latest Published' },
+                  { label: 'Manage Posts' },
+                ]}
+                tabStyle={{
+                  bgColor: '#E6E8EB',
+                  selectedBgColor: '#ffffff',
+                  color: 'rgba(0,0,0,0.87)',
+                }}
+                tabProps={{
+                  disableRipple: true,
+                }}
+                value={index}
+                onChange={(e, i) => setIndex(i)}
+              />
+            </Toolbar>
+          </AppBar>
         </div>
-    </div>
-  );
-};
+          <div>
+            {
+              recipe.map((recipe, i) => {
+                return(
+                  <p>
+                    {recipe.name}
+                  </p>
+                )
+              })
+            }
+          </div>
+      </div>
+    );
+  }; 
